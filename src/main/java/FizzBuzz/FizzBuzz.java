c9545d00e98400a4da92585a63444eed57a87edd
Kirill <fef1911@yandex.ru>
2023-11-04 12:05:21 +0300

    initial commit

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -1,2 +1,22 @@
-package FizzBuzz;public class FizzBuzz {
+package FizzBuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.println("bizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                System.out.println("buzz");
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+    }
 }
--- src/main/java/MultiplicateTable/MultiplicationTable.java
+++ src/main/java/MultiplicateTable/MultiplicationTable.java
@@ -1,3 +1,5 @@
+package MultiplicateTable;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
         for (int firstDigit = 1; firstDigit <= 10; firstDigit++) {
