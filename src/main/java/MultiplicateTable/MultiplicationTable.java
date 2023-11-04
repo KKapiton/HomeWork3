943049edd26e022fd4e0f507b995c8aa59944fdf
Kirill <fef1911@yandex.ru>
2023-11-04 22:20:35 +0300

    inintal commnit

--- src/main/java/MultiplicateTable/MultiplicationTable.java
+++ src/main/java/MultiplicateTable/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         for (int firstDigit = 1; firstDigit <= 10; firstDigit++) {
             for (int secondDigit = 1; secondDigit <= 10; secondDigit++) {
-                System.out.print(secondDigit * firstDigit + " ");
+                System.out.print(secondDigit * firstDigit + "\t");
             }
             System.out.println(" ");
         }
