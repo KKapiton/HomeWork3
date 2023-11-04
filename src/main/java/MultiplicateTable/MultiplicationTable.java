e45ee48ffa52be3ef0321cd5722baf7dbdc7ce4b
Kirill <fef1911@yandex.ru>
2023-11-04 12:04:32 +0300

    Initial commit

---
+++ src/main/java/MultiplicateTable/MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int firstDigit = 1; firstDigit <= 10; firstDigit++) {
+            for (int secondDigit = 1; secondDigit <= 10; secondDigit++) {
+                System.out.print(secondDigit * firstDigit + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/main/java/SimpleNumber/SimpleNum.java
@@ -0,0 +1,2 @@
+package SimpleNumber;public class SimpleNum {
+}
